164dba67342416d23154b2027ce7a0a9a03dc75a
QianrenLi <908559578@qq.com>
2024-05-12 22:15:48 +0800

    chore: fix installation script error

--- install.py
+++ install.py
@@ -8,18 +8,37 @@ def create_folders():
     for folder in folders:
         if not os.path.exists(folder):
             os.mkdir(folder)
+            
 def execute_commands():
     # Execute commands
     subprocess.run(["cargo", "build", "--release"], cwd="stream-replay")
     subprocess.run(["cargo", "build", "--release"], cwd="stream-replay-rx")
+    
+def pip_install():
+    # if pip is not installed, install it
+    try:
+        subprocess.run(["pip", "--version"])
+    except FileNotFoundError:
+        subprocess.run(["sudo", "apt", "install", "python3-pip", "-y"])
+        
+    ## install required packages
+    with open("requirements.txt") as f:
+        packages = f.read().splitlines()
+    for package in packages:
+        subprocess.run(["pip", "install", package])    
+        
+def cargo_install():
+    try:
+        subprocess.run(["cargo", "--version"])
+    except FileNotFoundError:
+        subprocess.run(["curl", "--proto", "=https", "--tlsv1.2", "-sSf", "https://sh.rustup.rs", "|", "sh"])
+        
 
 def main():
     create_folders()    
-    if subprocess.run(["cargo", "--version"]).returncode != 0:
-        subprocess.run(["curl", "--proto", "=https", "--tlsv1.2", "-sSf", "https://sh.rustup.rs", "|", "sh"])
-        return
-    
+    cargo_install()
     execute_commands()
+    pip_install()
 
 if __name__ == "__main__":
     main()
