49c0d68f38ca4b1addba58a72e46391657cbe5a1
QianrenLi <908559578@qq.com>
2024-05-17 19:51:29 +0800

    update: transmission submodule

--- run_exp.py
+++ run_exp.py
@@ -9,50 +9,86 @@ def exp_2024_4_6():
     sys.path.append('./expSrc/2024-4-6/channelRTTsamSumThru')
     ## run exp
     import exp
+
 def exp_2024_4_29_1():
     import sys
     sys.path.append('./expSrc/2024-4-29/experiment1')
     import exp
-    
+
 def exp_2024_4_29_2():
     import sys
     sys.path.append('./expSrc/2024-4-29/experiment2')
     import exp
-    
+
 def exp_2024_4_29_3():
     import sys
     sys.path.append('./expSrc/2024-4-29/experiment3')
     import exp
-    
+
 def exp_2024_4_29_4():
     import sys
     sys.path.append('./expSrc/2024-4-29/experiment4')
     import exp
-    
+
 def exp_2024_5_5():
     import sys
     sys.path.append('./expSrc/2024-5-5/experiment1')
     ## run exp
     import exp
-    
+
 def exp_2024_5_7():
     import sys
     sys.path.append('./expSrc/2024-5-5/experiment2')
     ## run exp
     import exp
-    
+
 def exp_2024_5_8():
     import sys
     sys.path.append('./expSrc/2024-5-7/experiment1')
     ## run exp
     import exp
-    
+
 def exp_2024_5_8_2():
     import sys
     sys.path.append('./expSrc/2024-5-7/experiment2')
     ## run exp
     import exp
-    
 
-exp_2024_5_8_2()
-# exp_2024_5_7()
+def exp_2024_5_9():
+    import sys
+    sys.path.append('./expSrc/2024-5-9/experiment1')
+    ## run exp
+    import exp
+
+def exp_2024_5_13():
+    import sys
+    sys.path.append('./expSrc/2024-5-13/experiment1')
+    ## run exp
+    import exp
+
+def exp_2024_5_13_2():
+    import sys
+    sys.path.append('./expSrc/2024-5-13/experiment2')
+    ## run exp
+    import exp
+
+def exp_2024_5_14():
+    import sys
+    sys.path.append('./expSrc/2024-5-14/experiment1')
+    ## run exp
+    import exp
+
+def exp_2024_5_16():
+    import sys
+    sys.path.append('./expSrc/2024-5-16/experiment2')
+    ## run exp
+    import exp
+
+def exp_2024_5_17():
+    import sys
+    sys.path.append('./expSrc/2024-5-17/experiment1')
+    ## run exp
+    import exp
+
+# exp_2024_5_14()
+exp_2024_5_17()
